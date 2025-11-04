18650255c7d8b3a4e555b3d8d3740686a9b8478a
Bhagyashri Roopesh <bhagyashriroopesh@gmail.com>
2025-11-04 23:18:09 +0400

    Initial structure setup

---
+++ lessons.js
@@ -0,0 +1,13 @@
+// lessons.js
+const lessons = [
+  { id: 1, subject: 'Mathematics', location: 'Edinburgh', price: 12.5, spaces: 5, icon: 'fa-solid fa-calculator', rating: 4 },
+  { id: 2, subject: 'English', location: 'Oxford', price: 11.0, spaces: 5, icon: 'fa-solid fa-book', rating: 3 },
+  { id: 3, subject: 'Physics', location: 'Cambridge', price: 15.0, spaces: 5, icon: 'fa-solid fa-atom', rating: 5 },
+  { id: 4, subject: 'Chemistry', location: 'London', price: 11.5, spaces: 5, icon: 'fa-solid fa-flask', rating: 2 },
+  { id: 5, subject: 'French', location: 'York', price: 13.5, spaces: 5, icon: 'fa-solid fa-language', rating: 4 },
+  { id: 6, subject: 'Dance', location: 'Glasgow', price: 15.0, spaces: 5, icon: 'fa-solid fa-music', rating: 5 },
+  { id: 7, subject: 'Music', location: 'Liverpool', price: 12.0, spaces: 5, icon: 'fa-solid fa-music', rating: 3 },
+  { id: 8, subject: 'Geography', location: 'Manchester', price: 9.0, spaces: 5, icon: 'fa-solid fa-globe', rating: 4 },
+  { id: 9, subject: 'Computer', location: 'Bath', price: 18.0, spaces: 5, icon: 'fa-solid fa-laptop-code', rating: 5 },
+  { id: 10, subject: 'Art', location: 'Leicester', price: 10.0, spaces: 5, icon: 'fa-solid fa-paint-brush', rating: 2 }
+];
